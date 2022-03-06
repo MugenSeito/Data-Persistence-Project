3d7214541c286bc0b2ec801bf31d6d56cfaa4822
Jazer Salazar <jazersalazar@gmail.com>
2022-03-06 23:08:51 +0800

    feat: add dynamic player name

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -36,6 +36,7 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+        AddPoint(0);
     }
 
     private void Update()
@@ -65,7 +66,7 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
+        ScoreText.text = $"{GameManager.Instance.playerName} Score : {m_Points}";
     }
 
     public void GameOver()
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 #if UNITY_EDITOR
     using UnityEditor;
 #endif
 
 public class MenuUIHandler : MonoBehaviour
 {
+    public GameObject nameInput;
+
     public void StartNew()
     {
+        GameManager.Instance.playerName = nameInput.GetComponent<TMP_InputField>().text;
         SceneManager.LoadScene(1);
     }
 
@@ -20,10 +24,5 @@ public class MenuUIHandler : MonoBehaviour
         #else
             Application.Quit();
         #endif
-    }
-
-    public void MainMenu()
-    {
-        SceneManager.LoadScene(0);
     }
 }
